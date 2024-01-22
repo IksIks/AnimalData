1e4ad94d4f59bfa39d812090dbe0157d412fbfbd
Konstantin <iksiks30@gmail.com>
2024-01-22 21:12:43 +0300

    Создан шаблн MVVM

--- AnimalData/App.xaml.cs
+++ AnimalData/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AnimalData
 {
@@ -10,5 +8,4 @@ namespace AnimalData
     public partial class App : Application
     {
     }
-
 }
