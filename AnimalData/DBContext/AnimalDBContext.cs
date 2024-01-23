997e8525ab29fab3b60da9dc6db8f9a6306f2cec
Konstantin <iksiks30@gmail.com>
2024-01-23 23:49:08 +0300

    Создал основыне классы животных, абстракный класс для них. Не доделал класс для создания базы данных

---
+++ AnimalData/DBContext/AnimalDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnimalData.DBContext
+{
+    internal class AnimalDBContext : DbContext
+    {
+        public AnimalDBContext()
+        {
+        }
+
+        public AnimalDBContext(DbContextOptions<AnimalDBContext> options) : base(options)
+        {
+        }
+
+        //public virtual DbSet<>
+    }
+}
---
+++ AnimalData/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AnimalData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalData.ViewModel
+{
+    internal class MainWindowViewModel
+    {
+        private ChordalClass dsd = new Bird();
+
+        public MainWindowViewModel()
+        {
+            dsd.AnimalName = "vc";
+        }
+    }
+}
