e6eaa2665fefd71176ddd9b048fb24459ca7b6ef
Konstantin <iksiks30@gmail.com>
2024-01-28 01:33:10 +0300

    Предварительная расзметка

--- AnimalData/DBContext/AnimalDBContext.cs
+++ AnimalData/DBContext/AnimalDBContext.cs
@@ -7,16 +7,17 @@ namespace AnimalData.DBContext
     {
         public AnimalDBContext()
         {
+            Database.EnsureCreated();
         }
 
         public AnimalDBContext(DbContextOptions<AnimalDBContext> options) : base(options)
         {
         }
 
-        public virtual DbSet<ChordalType> ChordalClasses { get; set; }
+        public virtual DbSet<ChordalType> ChordalTypes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-             => optionsBuilder.UseNpgsql("Host=localhost;UserName=postgres;Password=1;Database=TestAnimal");
+             => optionsBuilder.UseNpgsql("Host=localhost;UserName=postgres;Password=1;Database=AnimalsDB");
     }
 
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
