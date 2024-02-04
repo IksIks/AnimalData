eff8469cfc1f18db413d1a0e8a857b528c2a6975
Konstantin <iksiks30@gmail.com>
2024-02-04 14:20:54 +0300

    Добавил класс и настройки строки подключения

---
+++ AnimalData/Connection/ConnectString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Windows;
+
+namespace AnimalData.Connection
+{
+    internal class ConnectString
+    {
+        public static string GetConnectionString()
+        {
+            IConfigurationRoot configaration;
+            var builder = new ConfigurationBuilder();
+            try
+            {
+                configaration = builder.SetBasePath(Directory.GetCurrentDirectory() + "\\Connection").AddJsonFile("Connection.json").Build();
+                return configaration.GetConnectionString("PGSQL");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Отсутсвцет файл с настройками подключения к БД", "Внимание", MessageBoxButton.OK, MessageBoxImage.Error);
+                return "";
+            }
+            finally
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
